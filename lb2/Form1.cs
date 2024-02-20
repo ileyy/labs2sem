d48ebc335ed6c1b3d3b15df47f0f228c08c4455b
ileyy <khorkov.ilya2005@gmail.com>
2024-02-20 21:23:05 +0300

    lb2_need_test/fix

--- lb2/Form1.cs
+++ lb2/Form1.cs
@@ -8,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using System.Runtime.InteropServices;
+using File = System.IO.File;
 
 namespace lb2
 {
@@ -64,78 +67,106 @@ namespace lb2
         }
 
         private void работа1ToolStripMenuItem_Click(object sender, EventArgs e) {
-            FileStream f, g;
-            char fn, gn;
+            FileStream f = null, g = null;
+            string fn = "";
             int nomer, o1, o2, o3, i = 0;
-            //char gr[9], fam[13];
+            char[] gr = new char[9];
+            char[] fam = new char[13];
             float sr;
             OpenFileDialog openFile1 = new OpenFileDialog();
             SaveFileDialog saveFile1 = new SaveFileDialog();
+
             if (openFile1.ShowDialog() == DialogResult.OK && openFile1.FileName.Length > 0) {
-                fn = (char)(void)MarshalAs.StringToHGlobalAnsi(
-                openFile1.FileName.ToString());
-                if ((f = File.OpenRead()) == NULL)
-                    MessageBox::Show("Ошибка открытия файла ");
-                else
-                {
-                    if (saveFile1->ShowDialog()
-                    == System::Windows::Forms::DialogResult::OK
-                    && saveFile1->FileName->Length > 0)
-                    {
-                        gn = (char*)(void*)Marshal::StringToHGlobalAnsi(
-                        saveFile1->FileName->ToString());
-                        if ((g = fopen(gn, "w")) == NULL)
-                            MessageBox::Show("Ошибка открытия файла");
-                        else
-                        {
-                            richTextBox1->Clear();
-                            while (fscanf(f, "%d %s %s %d %d %d"
-                            , &nomer, gr, fam, &o1, &o2, &o3) > 0)
-                            {
-                                sr = (o1 + o2 + o3) / 3.0f;
-                                if (sr > 4)
-                                {
-                                    for (int j = strlen(fam); j < 10; j++)
-                                        //добавьте # include "string.h"
-                                        strcat(fam, " ");
-                                    String ^ s = gcnew String("");
-                                    //Преобразование строки с 0
-                                    //в конце к типу String
-                                    String ^ q = gcnew String(gr);
-                                    String ^ p = gcnew String(fam);
-                                    //nomer автоматически преобразуется
-                                    //к типу String
-                                    s = s + nomer;
-                                    if (s->Length == 1)
-                                        s = " " + s;
-                                    s = s + " " + q + " " + p + " " + o1 + " " + o2
-                                    //"\n" переводят строку в richtextBox
-                                    + " " + o3 + " " + sr + "\n";
-                                    //Добавить строку в richtextBox
-                                    this->richTextBox1->AppendText(s);
-                                    i++;
-                                    fprintf(g, "%2d %8s %10s%2d%2d%2d
-                                    8.2f\n”
-, i, gr, fam, o1, o2, o3, sr);
-                                }// if (sr > 4)
-                            }// while (fscanf ///
-                        }// else
-                    }// if( saveFile1->...
-                    fclose(f);
-                    fclose(g);
-                }// else
-            }// if ( openFile1->
-        }// private: System::Void работа1ToolStripMenuItem_Click(...)
-    }
+                fn = openFile1.FileName;
+                try {
+                    f = new FileStream(fn, FileMode.Open);
+                    StreamReader reader = new StreamReader(f);
+                    StreamWriter writer = new StreamWriter(g);
+                    string line;
+                    while ((line = reader.ReadLine()) != null) {
+                        string[] parts = line.Split(' ');
+                        nomer = int.Parse(parts[0]);
+                        gr = parts[1].ToCharArray();
+                        fam = parts[2].ToCharArray();
+                        o1 = int.Parse(parts[3]);
+                        o2 = int.Parse(parts[4]);
+                        o3 = int.Parse(parts[5]);
+                        sr = (o1 + o2 + o3) / 3.0f;
 
-        private void работа2ToolStripMenuItem_Click(object sender, EventArgs e) {
-
+                        if (sr > 4) {
+                            richTextBox1.AppendText(line + "\n");
+                            i++;
+                            string formattedLine = $"{i,2} {new string(gr),8} {new string(fam),10} {o1,2} {o2,2} {o3,2} {sr,8:f2}\n";
+                            writer.WriteLine(formattedLine);
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Ошибка открытия файла: " + ex.Message);
+                }
+                finally {
+                    if (f != null) f.Close();
+                    if (g != null) g.Close();
+                }
+            }
         }
 
-        
+
+        private void работа2ToolStripMenuItem_Click(object sender, EventArgs e) {
+            FileStream f = null, g = null;
+            string fn = "";
+            int nomer, o1, o2, o3, i = 0;
+            char[] gr = new char[9];
+            char[] fam = new char[13];
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+
+            if (openFile1.ShowDialog() == DialogResult.OK && openFile1.FileName.Length > 0) {
+                fn = openFile1.FileName;
+                try {
+                    f = new FileStream(fn, FileMode.Open);
+                    StreamReader reader = new StreamReader(f);
+                    string line;
+                    StreamWriter writer = new StreamWriter(g);
+                    while ((line = reader.ReadLine()) != null) {
+                        string[] parts = line.Split(' ');
+                        nomer = int.Parse(parts[0]);
+                        gr = parts[1].ToCharArray();
+                        fam = parts[2].ToCharArray();
+                        o1 = int.Parse(parts[3]);
+                        o2 = int.Parse(parts[4]);
+                        o3 = int.Parse(parts[5]);
+                        writer.WriteLine($" {new string(fam),8} {o1,2}{o2,2}{o3,2}");
+                        if (o1 >= 4 && o2 >= 4 && o3 >= 4) {
+                            string p = new string(fam);
+                            p = p + " " + o1 + " " + o2 + " " + o3 + "\n";
+                            richTextBox1.AppendText(p);
+                            writer.WriteLine($" {new string(fam),8} {o1,2}{o2,2}{o3,2}");
+                            i++;
+                        }
+                    }
+                    if (i == 0) writer.WriteLine("нет\n");
+                }
+                catch (Exception ex) {
+                    MessageBox.Show("Ошибка открытия файла: " + ex.Message);
+                }
+                finally{
+                    if (f != null) f.Close();
+                    if (g != null) g.Close();
+                }
+            }
+        }
+
+
 
         private void выходToolStripMenuItem_Click(object sender, EventArgs e) {
-
+            if (flag) {
+                DialogResult result;
+                result = MessageBox.Show(this, "Файл изменен.Сохранить?", "Ошибка", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+                if (result == DialogResult.Yes) сохранитьToolStripMenuItem_Click(sender, e);
+                flag = false;
+            }
+            Close();
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e) {
